789f514476ad60363d5fc3ce2224554425eb0f79
Lastranger28 <144068808+Lastranger28@users.noreply.github.com>
2024-08-16 08:28:24 -0400

    update

--- Program.cs
+++ Program.cs
@@ -1,7 +1,24 @@
 ﻿using FundamentosDotNet.Models;
 
-Pessoa pessoa1 = new Pessoa();
+/* Pessoa pessoa1 = new Pessoa();
 
 pessoa1.Nome = "André";
 pessoa1.Idade = 33;
 pessoa1.Apresentar();
+*/ 
+
+
+int quantidadeEmEstoque = 10;
+int quantidadeCompra = 4;
+bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
+
+
+Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
+Console.WriteLine($"Quantidade da compra: {quantidadeCompra}");
+Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
+
+if (possivelVenda){
+    Console.WriteLine("Venda realizada.");
+} else {
+    Console.WriteLine("Desculpe, não temos a quantidade desejada em estoque.");
+}
