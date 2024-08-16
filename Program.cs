ee973c9724da61fc30beae085b03b5bc24301e01
Lastranger28 <144068808+Lastranger28@users.noreply.github.com>
2024-08-16 08:38:04 -0400

    update

--- Program.cs
+++ Program.cs
@@ -9,15 +9,18 @@ pessoa1.Apresentar();
 
 
 int quantidadeEmEstoque = 10;
-int quantidadeCompra = 4;
-bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
+int quantidadeCompra = 0;
+bool possivelVenda = quantidadeCompra > 0 && quantidadeEmEstoque >= quantidadeCompra;
 
 
 Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
 Console.WriteLine($"Quantidade da compra: {quantidadeCompra}");
 Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
 
-if (possivelVenda){
+
+if (quantidadeCompra == 0){
+    Console.WriteLine("Venda inválida");
+}else if (possivelVenda){
     Console.WriteLine("Venda realizada.");
 } else {
     Console.WriteLine("Desculpe, não temos a quantidade desejada em estoque.");
