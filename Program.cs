0faac01aa738d96d327880a41dac3e4f7850261d
Lastranger28 <144068808+Lastranger28@users.noreply.github.com>
2024-09-17 08:52:18 -0400

    update 17-09-24

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using FundamentosDotNet.Models;
 
+
 /* Pessoa pessoa1 = new Pessoa();
 
 pessoa1.Nome = "André";
@@ -46,6 +47,7 @@ switch (letra){
         break;
 } 
 
+
 Calculadora calc = new Calculadora();
 calc.Somar(2, 5);
 calc.Subtrair(10, 2);
@@ -56,4 +58,56 @@ calc.Seno(30);
 calc.Coseno(30);
 calc.Tangente(30); 
 calc.RaizQuadrada(9);
+
+
+int soma = 0, numero = 0;
+
+do{
+    Console.WriteLine("Digite um numero (0 para parar):");
+    numero = Convert.ToInt32(Console.ReadLine());
+
+    soma += numero;
+}while(numero != 0);
+
+
+Console.WriteLine($"Sua soma é: {soma}");
 */
+
+string opcao;
+bool exibirMenu = true;
+
+while(exibirMenu)
+{
+    Console.WriteLine("Digite sua opção");
+    Console.WriteLine("1 - Cadastrar cliente");
+    Console.WriteLine("2 - Buscar cliente");
+    Console.WriteLine("3 - Apagar cliente");
+    Console.WriteLine("4 - Encerrar");
+
+    opcao = Console.ReadLine();
+    
+
+    switch(opcao)
+    {
+        case "1":
+            Console.WriteLine("Cadastro de Cliente");
+            break;
+        
+        case "2":
+            Console.WriteLine("Busca de Cliente");
+            break;
+        
+        case "3":
+            Console.WriteLine("Apagar Cliente");
+            break;
+        
+        case "4":
+            Console.WriteLine("Encerrado");
+            exibirMenu = false;
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+}
